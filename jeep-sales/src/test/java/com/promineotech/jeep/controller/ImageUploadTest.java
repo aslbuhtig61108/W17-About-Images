2e9e1dd0f62e6857843be14b1423e718c75c0fa6
Ed Locquiao <nilatian.15@gmail.com>
2022-11-07 23:02:38 -0800

    Second commit: currently on 2nd vid ts@07:31

--- jeep-sales/src/test/java/com/promineotech/jeep/controller/ImageUploadTest.java
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/ImageUploadTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,6 +16,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -21,6 +26,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Sql(
+    scripts = {"classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+        "classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
+    config = @SqlConfig(encoding = "utf-8"))
+
 class ImageUploadTest {
 
   private static final String JEEP_IMAGE = "Jeep Wrangler Rubicon.jpg";
@@ -31,11 +42,26 @@ class ImageUploadTest {
   // Let's check if it actually wrote to the database
   @Autowired
   private JdbcTemplate jdbcTemplate; 
-  
+ 
   @Test
   void testThatTheServerCorrectlyReceivesAnImageAndReturnsAnOKResponse() throws Exception {
     
+    // Check to see if image was added to the database
+    String json = assertImageUpload();
+    String imageId = extractImageId(json);
     
+    System.out.println("json=" + json);
+    System.out.println("imageId=" + imageId);
+  }
+
+  private String extractImageId(String json) {
+    String[] parts = json.substring(1, json.length()-1).split(":");
+    return parts[1].substring(1,parts[1].length() - 1);
+    
+  }
+
+  protected String assertImageUpload() throws IOException, Exception, UnsupportedEncodingException {
+    int numRows = JdbcTestUtils.countRowsInTable(jdbcTemplate, "images");
 
     Resource image = new ClassPathResource(JEEP_IMAGE);
 
@@ -54,14 +80,16 @@ class ImageUploadTest {
           .perform(MockMvcRequestBuilders
               .multipart("/jeeps/1/image")
               .file(file))
-          .andDo(print())
           .andExpect(status().is(201))
           .andReturn();
       // formatter: on
  
       String content = result.getResponse().getContentAsString();
       assertThat(content).isNotEmpty();
+      assertThat(JdbcTestUtils.countRowsInTable(jdbcTemplate, "images"))
+        .isEqualTo(numRows + 1);
+      
+      return content;
     }
- 
   }
 }
