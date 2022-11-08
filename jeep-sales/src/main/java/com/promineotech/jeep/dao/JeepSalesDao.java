f21a1cd35794778ab334c45d123fbbe2ab43cb3c
Ed Locquiao <nilatian.15@gmail.com>
2022-11-07 22:08:10 -0800

    First commit: W17 uploading images complete and saving images is in progress

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/dao/JeepSalesDao.java
@@ -0,0 +1,21 @@
+package com.promineotech.jeep.dao;
+
+import java.util.List;
+import com.promineotech.jeep.entity.Image;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+// W15 - Coding Assignment - ITEM#1a and b. - create this interface in the com.promineotech.jeep.dao package
+public interface JeepSalesDao {
+
+	/**
+	 * 
+	 * @param model
+	 * @param trim
+	 * @return
+	 */
+	List<Jeep> fetchJeeps (JeepModel model, String trim);
+
+  void saveImage(Image image);
+  
+}
